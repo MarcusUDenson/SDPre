636f7a09184b58623316e2f2142e20fd4e353533
unknown <144676522+MarcusUDenson@users.noreply.github.com>
2025-05-23 09:44:20 -0400

    Objective2Lab2.java

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+
+    /*
+      Name: Marcus Denson Age: 31 State: Georgia
+    */
+    
+  }
+}
