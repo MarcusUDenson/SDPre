8d9d4cefdc8d376ae82c7361ad4e7c6e34447847
unknown <144676522+MarcusUDenson@users.noreply.github.com>
2025-05-23 08:38:46 -0400

    Objective2Lab1.java

---
+++ Labs/Objective2Lab1.java
@@ -0,0 +1,11 @@
+public class Objective2Lab1 {
+  public static void main(String[] args) {
+    String hello; // declaring the variable named "hello"
+     hello = "Hello World"; // initializing the variable named "hello"
+
+    /*
+      Your solution goes here
+    */
+    
+  }
+}
