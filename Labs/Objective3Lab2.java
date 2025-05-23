c728f3859abb88600b3817bcba8e097647237431
unknown <144676522+MarcusUDenson@users.noreply.github.com>
2025-05-23 11:48:25 -0400

    Objective3Lab2.java

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,7 @@
+public class Objective3Lab2 {
+    public static void main(String[] args) {
+    double num = Math.sqrt(x * x);    //num => a^2
+    double result = Math.sqrt(x^2) + Math.sqrt(y^2); //result => a^2 + b^2 => c^2
+}
+    }
+
