c35d299cd0f4987e9f88b3c2353cb04f2d819b5a
unknown <144676522+MarcusUDenson@users.noreply.github.com>
2025-05-24 08:14:25 -0400

    Objective9Lab3.java

---
+++ Labs/Objective9Lab3.java
@@ -0,0 +1,45 @@
+public class ObjectiveLab3 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+    while(true){
+
+      /*
+      _____Menu_____
+1: Say Hello
+2: List My favorite foods
+3: Exit
+
+1
+Hello Human
+_____Menu_____
+1: Say Hello
+2: List My favorite foods
+3: Exit
+
+2
+Apples, Bananas, Coconuts
+_____Menu_____
+1: Say Hello
+2: List My favorite foods
+3: Exit
+
+3
+Goodbye
+      */
+      
+    }
+
+    scanner.close();
+
+  }
+
+  public static void printMenu() {
+    System.out.println("_____Menu_____");
+    System.out.println("1: Say Hello");
+    System.out.println("2: List My favorite foods");
+    System.out.println("3: Exit");
+    System.out.println();
+  }
+}
