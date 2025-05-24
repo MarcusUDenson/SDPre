9180a87b14f8dc3792d8f739a4511de09c7eeb9a
unknown <144676522+MarcusUDenson@users.noreply.github.com>
2025-05-24 07:52:57 -0400

    Objective9Lab1.java

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,13 @@
+public class Objective9Lab1 {
+  public static void main(String[] args) {
+
+    /*
+    Hello Marcus
+    */  
+    
+  }
+
+  public static void printName() {
+    System.out.println("Hello Doug");
+  }
+}
