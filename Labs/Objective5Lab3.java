93364676a04c8ae9eeaccca71de7c6fcee67ecd3
unknown <144676522+MarcusUDenson@users.noreply.github.com>
2025-05-24 06:16:33 -0400

    Objective5Lab3.java

---
+++ Labs/Objective5Lab3.java
@@ -0,0 +1,14 @@
+public class Objective5Lab3 {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int userNum;
+
+      System.out.println("Please enter a number: ");
+
+      /*
+        3
+      */
+
+      scanner.close();
+  }
+}
