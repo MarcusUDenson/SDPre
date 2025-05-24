965c56c3820f911d2c207c16264b4365ac63a831
unknown <144676522+MarcusUDenson@users.noreply.github.com>
2025-05-24 06:06:55 -0400

    Objective5Lab1.java

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,21 @@
+public class Objective5Lab1 {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 15;
+
+    /*
+      Greater than or lesser than statements
+    */
+
+    if(num1 > num2) {
+      /*
+        5 is greater than 15
+      */
+    }
+    else if(num2 > num1) {
+      /*
+        15 is greater than 5
+      */
+    }
+  }
+}
