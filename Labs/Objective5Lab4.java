00a2eb00219003ef093a6b4e975820a58b1adbde
unknown <144676522+MarcusUDenson@users.noreply.github.com>
2025-05-24 06:24:29 -0400

    Objective5Lab4.java

---
+++ Labs/Objective5Lab4.java
@@ -0,0 +1,16 @@
+public class 
+ public static void main(String[] args) {
+  Scanner scanner = new Scanner(System.in);
+      int num = 6;
+
+if(num % 2 == 0){
+  //43;
+
+      System.out.println("Please enter a number: ");
+
+      /*
+        43
+      */
+
+      scanner.close();
+}
