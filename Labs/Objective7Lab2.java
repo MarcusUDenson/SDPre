34daac4bde30d161cf468c33e835a9d33dde8931
unknown <144676522+MarcusUDenson@users.noreply.github.com>
2025-05-24 06:41:42 -0400

    Objective7Lab2.java

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,11 @@
+public class Objective7Lab2 {
+  public static void main(String[] args) {
+  int counter = 0;
+
+    while(counter <= 20) {
+      /*
+      0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
+      */
+    }
+  }
+}
