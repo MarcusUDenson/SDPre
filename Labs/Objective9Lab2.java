ef205db9fb3db0da12a20dcc8c2bc79c477e316c
unknown <144676522+MarcusUDenson@users.noreply.github.com>
2025-05-24 08:06:26 -0400

    Objective9Lab2.java

---
+++ Labs/Objective9Lab2.java
@@ -0,0 +1,17 @@
+public class Objective9Lab2 {
+  public static void main(String[] args) {
+    System.out.println("In main");
+    printMessage();
+    System.out.println("In main");
+  }
+
+  public static void printMessage() {
+    /*
+
+    In main
+    In method
+    In main
+    */
+    
+  }
+}
