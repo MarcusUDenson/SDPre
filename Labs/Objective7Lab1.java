33d24776b43fb4a8b0fe90ef68e8ded0c9a46494
unknown <144676522+MarcusUDenson@users.noreply.github.com>
2025-05-24 06:36:29 -0400

    Objective7Lab1.java

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,14 @@
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+    int counter = 10;
+
+    while(counter > 0) {
+
+      /*
+      10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+      */
+
+      counter = counter - 1; 
+    }
+  }
+}
