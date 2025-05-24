ae99d97de314d00bc2819f2f317e61a3ff1a2a6c
unknown <144676522+MarcusUDenson@users.noreply.github.com>
2025-05-24 04:49:14 -0400

    Objective4Lab2.java

---
+++ Labs/Objective4Lab2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Objective4Lab2 {
+  public static void main(String[] args) {
+
+    Scanner keyboard = new Scanner(System.in);
+
+    int num1 = 0, num2 = 0, num3 = 0;
+    double dub1 = 0.0, dub2 = 0.0, dub3 = 0.0;
+
+    //>> 1 >> 7 >> 5
+
+    //>> 1.1 >> 2.2 >> 3.3
+
+    //1 + 7 + 5 = 13
+
+    //1.1 + 2.2 + 3.3 = 6.6
+
+  }
+}
