4f414f04ba34d909a5af2e0ec74ae030a4aa988f
unknown <144676522+MarcusUDenson@users.noreply.github.com>
2025-05-24 04:27:18 -0400

    Objective4Lab1.java

---
+++ Labs/Objective4Lab1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Objective4Lab1 {
+  public static void main(String[] args) {
+
+    Scanner keyboard = new Scanner(System.in);
+
+    String fname = "";
+    String lname = "";
+    String favoriteAnimal = "";
+    String favoriteFood = "";
+    String favoriteSong = "";
+
+    //What is your first name? >> Marcus What is your last name? >> Denson What is your favorite animal? >>Horse What is your favortie food? >>Vegetarian  What is your favorite song? >>Pharrell
+
+    //My name is Marcus Denson. My favorite animal is the horse. My favorite food is Vegetarian. MY favorite song is  Pharrell.
+
+  }
+}
